7598437e038b8c7c2d296e10c8c4e4fb4c071a2f
carusi99 <paolapachecocarusi@gmail.com>
2024-05-13 16:34:11 -0500

    [ADD] table users

--- src/db/migrations/2024.05.13T19.38.35.users.ts
+++ src/db/migrations/2024.05.13T19.38.35.users.ts
@@ -1,8 +1,16 @@
 import { Migration } from "../scripts/dbMigrate";
 
 export const up: Migration = async (params) => {
-  params.context.query(`RAISE EXCEPTION 'up migration not implemented'`);
+  return params.context.query(`CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    age INTEGER CHECK (age > 0),
+    role VARCHAR(20) NOT NULL DEFAULT 'user'
+);
+`);
 };
+
 export const down: Migration = async (params) => {
-  params.context.query(`RAISE EXCEPTION 'down migration not implemented'`);
+  return params.context.query(`DROP TABLE users;`);
 };
