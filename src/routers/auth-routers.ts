8dd4ad475417f049caef8c8b1cf98af39743a998
carusi99 <paolapachecocarusi@gmail.com>
2024-05-14 09:35:23 -0500

    [ADD] router post/signup in routers

--- src/routers/auth-routers.ts
+++ src/routers/auth-routers.ts
@@ -1,8 +1,33 @@
+
 import express from "express";
-import { validateCredentials } from "../services/auth-services";
+import { validateCredentials, createUser } from "../services/auth-services";
 import jwt from "jsonwebtoken";
-import { jwtSecret } from "../middlewares/authenticate";
+import {validationHandler} from "../middlewares/validation";
+import { userSchema } from "../models/auth-models";
+
+const jwtSecret = "ultra-mega-secret";
 const authRouter = express.Router();
+
+authRouter.post(
+  "/signup",
+  validationHandler(userSchema),
+  async (req, res, next) => {
+    try {
+      const newUser = await createUser(req.body);
+      res.status(201).json({
+        ok: true,
+        message: "Usuario registrado exitosamente",
+        data: {
+          id: newUser.id,
+          email: newUser.email,
+          role: newUser.role,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 
 authRouter.post("/login", async (req, res, next) => {
