401853fe11406650cb9a5ec52300f90918eab230
carusi99 <paolapachecocarusi@gmail.com>
2024-05-14 09:36:20 -0500

    [ADD] function createUser in services

--- src/services/auth-services.ts
+++ src/services/auth-services.ts
@@ -1,7 +1,39 @@
+import * as bcrypt from "bcrypt";
 import { User, UserParams } from "../models/auth-models";
 import * as db from "../data/auth-data";
-import * as bcrypt from "bcrypt";
 import { ApiError } from "../middlewares/error";
+
+export async function createUser(data: UserParams): Promise<User> {
+  const { email, password } = data;
+
+  // Verificar la longitud de la contraseña
+  if (password.length < 6) {
+      throw new ApiError("La contraseña debe tener al menos 6 caracteres", 400);
+  }
+
+  // Verificar la existencia del usuario
+  try {
+      const user = await db.getUserByName(email);
+      if (user) {
+          throw new ApiError("El email ya está registrado", 400);
+      }
+  } catch (error) {
+      throw new ApiError("Error al verificar la existencia del usuario", 500);
+  }
+
+  // Hashear la contraseña
+  const costFactor = 10;
+  const hashedPassword = await bcrypt.hash(password, costFactor);
+
+  // Crear el nuevo usuario
+  try {
+      const newUser = await db.createUsers({ ...data, password: hashedPassword });
+      return newUser;
+  } catch (error) {
+    console.error("Error al crear el usuario en la base de datos:", error);
+      throw new ApiError("Error al crear el usuario", 500);
+  }
+}
 
 
 
@@ -11,7 +43,7 @@ export async function validateCredentials(
   ): Promise<User> {
     const { email, password } = credentials;  
     const user = await db.getUserByName(email);
-    
+
     if (!user) {
       throw new ApiError("Credenciales incorrectas", 400);
     }
