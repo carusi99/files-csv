5f3d1d6534971a360a6b1b0d4fe6d90c16c81ed8
carusi99 <paolapachecocarusi@gmail.com>
2024-05-13 19:05:07 -0500

    [ADD] migration seed file was generated

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import userRouter from "./routers/auth-routers";
 
 if (process.env["NODE_ENV"] === "test") {
   configDotenv({ path: ".env.test" });
@@ -10,3 +11,4 @@ if (process.env["NODE_ENV"] === "test") {
 export const app = express();
 
 app.use(express.json());
+app.use(userRouter)
--- src/db/migrations/2024.05.13T19.38.35.users.ts
+++ src/db/migrations/2024.05.13T19.38.35.users.ts
@@ -4,6 +4,7 @@ export const up: Migration = async (params) => {
   return params.context.query(`CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     age INTEGER CHECK (age > 0),
     role VARCHAR(20) NOT NULL DEFAULT 'user'
