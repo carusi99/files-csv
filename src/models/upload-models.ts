9a82b06d89bbf594c180fc4302c87ad3892ca567
carusi99 <paolapachecocarusi@gmail.com>
2024-05-15 17:27:02 -0500

    [ADD] error handling completed

--- src/models/upload-models.ts
+++ src/models/upload-models.ts
@@ -9,3 +9,26 @@ export const clientSchema = z.object({
   email: z.string()
     .min(1, { message: "El campo 'email' no puede estar vacío." })
     .email({ message: "El formato del correo electrónico es inválido, falta @" }),
+    age: z.number()
+    .min(1, { message: "El campo 'age' debe ser un número mayor a 0"})
+    .int()
+    .positive({ message: "El campo 'age' debe ser un número positivo" })
+    .transform(value => {
+      const parsedValue = Number(value);
+      if (isNaN(parsedValue)) {
+        throw new Error("Solo se aceptan números en el campo 'age'.");
+      }
+      return parsedValue;
+    }),
+    role: z.enum(["admin", "user"], {
+        errorMap: () => ({ message: "El rol debe ser admin o user"}),
+      }).default("user"),
+});
+
+
+
+export type ClientParams = z.infer<typeof clientSchema>;
+
+export type Client = ClientParams & { id: number };
+
+
