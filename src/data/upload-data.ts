c702a7fcc3c94f26427d10e656990abf0901b799
carusi99 <paolapachecocarusi@gmail.com>
2024-05-15 17:24:53 -0500

    [ADD] finish of functions in upload-data

--- src/data/upload-data.ts
+++ src/data/upload-data.ts
@@ -8,3 +8,25 @@ export async function processCSV(filePath: string): Promise<{ success: any[], er
   const results: any[] = [];
   const errors: any[] = [];
   let rowNumber = 0; // Variable para llevar el número de fila
+
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(parser)
+      .on('data', (data) => {
+        rowNumber++; // Incrementa el número de fila para cada nueva fila
+        try {
+          data.age = Number(data.age);
+          const parsedData = clientSchema.parse(data);
+          results.push(parsedData);
+        } catch (error: any) {
+          errors.push({ row: rowNumber, details: error.errors });
+        }
+      })
+      .on('error', (error) => {
+        reject(error);
+      })
+      .on('end', () => {
+        resolve({ success: results, errors });
+      });
+  });
+}
